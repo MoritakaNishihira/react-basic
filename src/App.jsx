c6cb0ab3d378acc6ffa4cb929370a288c0e45c15
moritaka nishihira <moritaka.nishihira@gmail.com>
2024-04-02 22:55:11 +0900

    stateについて

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,97 @@
-import { ColofullMessage } from "./components/ColorfullMessage";
+import { useState } from "react";
+import { ColorfulMessage } from "./components/ColorfulMessage";
 
 export const App = () => {
-  const onClickButton = () => {
-    return alert("おした！");
+  const [arr, setArr] = useState([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]);
+  const [num, setNum] = useState(arr.length + 1);
+  const [rows, setRows] = useState("ここに表を出します");
+  const [isDisp, setIsDisp] = useState("block");
+  const tableStyles = {
+    table: {
+      border: "solid 2px",
+      borderCollapse: "collapse",
+    },
+    th: {
+      border: "solid 2px",
+      backgroundColor: "yellow",
+      padding: "0px 10px",
+    },
+    tr: { border: "solid 1px" },
+    td_no: {
+      border: "solid 1px",
+      borderRight: "solid 2px",
+    },
+    td_str: { border: "solid 1px" },
   };
+  const onClickCreateTable = () => {
+    setRows(() => {
+      setIsDisp((prev) => {
+        return (prev = "block");
+      });
+      setNum((prev) => prev + 1);
+      setArr((prev) => [...prev, num]);
+      return (
+        <div>
+          <p>表が出ました。ボタンをクリックすると行が増えます。</p>
+          <table id="tbl" style={tableStyles.table}>
+            <thead id="th">
+              <th id="th_row_no" style={tableStyles.th}>
+                rowNo
+              </th>
+              <th id="th_row_str" style={tableStyles.th}>
+                rowString
+              </th>
+            </thead>
+            <tbody id="tb">
+              {arr.map((str, idx) => {
+                return (
+                  <tr id={"tr_row_" + Number(idx + 1)} style={tableStyles.tr}>
+                    <td
+                      id={"td_row_no_" + Number(idx + 1)}
+                      style={tableStyles.td_no}
+                    >
+                      <div>{idx + 1}</div>
+                    </td>
+                    <td
+                      id={"td_row_str_" + Number(idx + 1)}
+                      style={tableStyles.td_str}
+                    >
+                      <div>
+                        <ColorfulMessage
+                          fontColor="blue"
+                          context={str}
+                        ></ColorfulMessage>
+                      </div>
+                    </td>
+                  </tr>
+                );
+              })}
+            </tbody>
+          </table>
+        </div>
+      );
+    });
+  };
+
   return (
     <div>
       <h1 style={{ color: "red" }}>おはこんにちは</h1>
       <div>
-        <ColofullMessage
+        <ColorfulMessage
           fontColor="blue"
           context="お元気ですか？"
-        ></ColofullMessage>
-        <ColofullMessage
+        ></ColorfulMessage>
+        <ColorfulMessage
           fontColor="red"
-          context="はい、元気ですよ！"
-        ></ColofullMessage>
+          context="はい、元気ですよ!"
+        ></ColorfulMessage>
       </div>
-      <button onClick={onClickButton}>ボタン</button>
+      <p></p>
+      <button onClick={onClickCreateTable} style={{ display: isDisp }}>
+        Display Table !!
+      </button>
+      <p></p>
+      {rows}
     </div>
   );
 };
